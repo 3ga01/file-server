0bbd6b579039e30163a7e8d7fa006228e5920404
3ga01 <omariemmanuel91@gmail.com>
2023-03-30 01:30:28 +0000

    User feed added

--- src/main/java/company/trial/Controller.java
+++ src/main/java/company/trial/Controller.java
@@ -1,14 +1,10 @@
 package company.trial;
 
-import java.net.URI;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
