dcf71efc429da52b213daa83de854a87d7808df7
3ga01 <omariemmanuel91@gmail.com>
2023-04-08 02:06:32 +0000

    ...

--- src/main/java/company/trial/repositories/FileRepository.java
+++ src/main/java/company/trial/repositories/FileRepository.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface FileRepository extends JpaRepository<Files, Long> {
   Optional<Files> findByName(String name);
 
-
   List<Files> findByNameContainingIgnoreCase(String name);
-  
-  
+
 }
