ea1a1caa93132759483bfb53d327f0aeb10feebb
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-05-13 01:02:56 +0000

    spring security working

--- src/main/java/company/trial/controllers/UserController.java
+++ src/main/java/company/trial/controllers/UserController.java
@@ -3,6 +3,7 @@ package company.trial.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,15 +18,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    
     @PostMapping("/user/signUp")
     public ModelAndView signupSubmit(@ModelAttribute("user") User user,
             BindingResult result, Model model) {
         if (result.hasErrors()) {
             return new ModelAndView("signUp");
         }
-       if (userService.userExist(user.getEmail())){
-           return new ModelAndView("signUp");
-       }
+        if (userService.userExist(user.getEmail())) {
+            return new ModelAndView("signUp");
+        }
         userService.saveUser(user);
         return new ModelAndView("verify");
     }
